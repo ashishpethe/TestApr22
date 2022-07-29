2f210f2f48e7ddc6ebfd298bc877d0a6f95dbe8a
ashishpethe <ashish.pethe1@gmail.com>
2022-07-29 08:39:09 +0530

    branch B conflict resolved

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -6,7 +6,8 @@ public class Class1 {
 
 		System.out.println("Test Class- Update branch A");
 		System.out.println("Project conflict branch A");
-
+		System.out.println("Test Class Branch B");
+		System.out.println("Test conflict Branch B");
 	}
 
 }
