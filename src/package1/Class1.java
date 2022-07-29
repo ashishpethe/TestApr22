12d582685469be2f5fa468a9832937ddaea9530b
ashishpethe <ashish.pethe1@gmail.com>
2022-07-29 08:15:17 +0530

    main method change

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,7 +3,9 @@ package package1;
 public class Class1 {
 
 	public static void main(String[] args) {
-		System.out.println("Test Class");
+
+		System.out.println("Test Class- Update branch A");
+		System.out.println("Project conflict branch A");
 
 	}
 
