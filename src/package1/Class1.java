7d0cd1f977f949edf66c17ded15b731ba9d64e89
ashishpethe <ashish.pethe1@gmail.com>
2022-07-29 08:47:38 +0530

    Branch A changes 23

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -9,6 +9,7 @@ public class Class1 {
 		
 		System.out.println("Project conflict branch A NEW Changes1");
 		System.out.println("Project conflict branch A NEW Changes2");
+		System.out.println("Project conflict branch A NEW Changes3");
 
 	}
 
