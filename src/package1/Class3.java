f6c4fd98855c1dfe34c59b8e599b7d7271d6128a
ashishpethe <ashish.pethe1@gmail.com>
2022-07-29 09:28:48 +0530

    Class3 updated for A3

---
+++ src/package1/Class3.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class Class3 {
+
+	public static void main(String[] args) {
+		System.out.println("Test Class3 for Branch A3");
+		System.out.println("Adding new code - A3");
+		System.out.println("Adding new code for Branch A3");
+	}
+
+}
